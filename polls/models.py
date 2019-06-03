0802420168d393c2b05705abb2ed48006b3ec77d
smzaidi <50637541+smzaidi@users.noreply.github.com>
2019-06-03 15:15:18 -0400

    Update models.py

--- polls/models.py
+++ polls/models.py
@@ -1,7 +1,9 @@
-from django.db import models
 import datetime
+
+from django.db import models
 from django.utils import timezone
-# Create your models here.
+
+
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
@@ -11,14 +13,28 @@ class Question(models.Model):
 
     def was_published_recently(self):
         now = timezone.now()
-        return now - datetime.timedelta(days = 1) <= self.pub_date <= now
-    was_published_recently.admin_order_field = 'pub_date'
-    was_published_recently.boolean = True
-    was_published_recently.short_description = 'Published recently?'
-    
+        return now - datetime.timedelta(days=1) <= self.pub_date <= now
+
+    def choices(self):
+        if not hasattr(self, '_choices'):
+            self._choices = self.choice_set.all()
+        return self._choices
+
+    def max_voted_choice(self):
+        if not hasattr(self, '_max_voted_choice'):
+            choices = self.choice_set.order_by('-votes')
+            if not choices:
+                self._max_voted_choice = None
+            else:
+                self._max_voted_choice = choices[0]
+        return self._max_voted_choice
+
+
+
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
+
     def __str__(self):
         return self.choice_text
